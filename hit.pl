3d87052f62be19aebee5fda7ff0a171fa6976711
Jari Luukkonen <Dragonius@users.noreply.github.com>
2015-11-27 13:10:45 +0200

    Copied Sibling Devel

--- hit.pl
+++ hit.pl
@@ -8,8 +8,8 @@ $other=0;
 $timeout=0;
 $local_hit=0;
 $local_miss=0;
-$remote_hit=0;
-$remote_miss=0;
+$udp_hit=0;
+$udp_miss=0;
 $tcp_hit=0;
 $tcp_miss=0;
 $direct=0;
@@ -31,6 +31,10 @@ while (<>) {
                 @F = split;
                 $L = $F[3];                  # local cache result code
                 $H = $F[8];                  # hierarchy code
+
+#$L 3 Poikki 3  ( yleensä tcp_hit/miss udp_hit/miss) 
+#$H 8 Leikkaus  ( yleensä Tcp tai Udp vastaus Yleensä Sibling tai onnistunut haku) 
+
 #We want also UDP for icp and htcp
 #               next unless ($L =~ /TCP_/);     # skip UDP and errors
                 if ($L =~ /UDP/) {
@@ -39,44 +43,56 @@ while (<>) {
                 $tcp++; }
                 $N++;
 
-                if ($H =~ /TIMEOUT_HIER/) {
-                $timeout++; }
-
 #For UDP HIT/MISS
                 if ($L =~ /UDP_HIT/) {
-                $remote_hit++;
+                        $udp_hit++;
                 } if ($L =~ /UDP_MISS/) {
-                $remote_miss++; }
+                        $udp_miss++; }
                 
-#$L 3 Poikki
-#$H 8 Leikkaus  ( yleensä Tcp tai Udp vastaus Yleensä Sibling tai onnistunut haku) 
+
 #Added TCP HIT/MISS
                 if ($L =~ /TCP_HIT/) {
-                $tcp_hit++;
-                } if ($L =~ /TCP_MISS/) {
-                $tcp_miss++; }
+                        $tcp_hit++;
+                } 
+                if ($L =~ /TCP_MISS/) {
+                        $tcp_miss++; }
 
                 if ($L =~ /IMS_HIT/) {
                         $ims_hit++;
-                } elsif ($L =~ /MEM_HIT/) {
+                } 
+                if ($L =~ /MEM_HIT/) {
                         $mem_hit++;
-                } elsif ($L =~ /ABORTED/) {
+                } 
+                if ($L =~ /ABORTED/) {
                         $aborted_hit++;
-                } elsif ($L =~ /REFRESH_UNMODIFIED/) {
+                } 
+                if ($L =~ /REFRESH_UNMODIFIED/) {
                         $unmodified++;
-                } elsif ($L =~ /REFRESH_MODIFIED/) {
+                } 
+                if ($L =~ /REFRESH_MODIFIED/) {
                         $modified++;
-                } elsif ($L =~ /NEGATIVE_HIT/) {
+                } 
+                if ($L =~ /NEGATIVE_HIT/) {
                         $negative++;
-                } elsif ($H =~ /HIT/) {
+                } 
+  #Sibling hit here. Must do more code so we want know it is a upd or tcp hit.
+  #time       0 192.168.XX.XX UDP_HIT/000 0 HTCP_TST http://website.com/7.jpg - HIER_NONE/- -
+  #time       2 192.168.XX.XX TCP_HIT/504 5105 GET http://website.com/7.jpg - HIER_NONE/- text/html
+                if ($H =~ /SIBLING_HIT/) {
                         $sibling_hit++;
-                } elsif ($L =~ /SIBLING_HIT/) {
-                        $local_hit++;
-                } elsif ($H =~ /HIER_DIRECT/) {
+                } 
+#Hier Return Code
+                if ($H =~ /HIER_DIRECT/) {
                         $direct++;
-                } elsif ($L =~ /MISS/) {
-                        $local_miss++;
-                } else {
+                }
+                if ($H =~ /TIMEOUT_HIER/) {
+                        $timeout++; 
+                }
+#               if ($L =~ /MISS/) {
+#                        $local_miss++;
+#                } 
+  #We need all others here for better hit accusary
+                        else {
                         $other++;
                 }
         }
@@ -89,10 +105,10 @@ while (<>) {
         printf "UDP-REQUESTS %d\n", $udp;
         printf "TIMEOUTS %d\n", $timeout;
         printf "TIMEOUT %% %f\n", 100*$timeout/$N;
-        printf "REMOTE-HIT %% %f\n", 100*$remote_hit/$udp;
-        printf "REMOTE-HIT %d\n", $remote_hit;
-        printf "REMOTE-MISS %% %f\n", 100*$remote_miss/$udp;
-        printf "REMOTE-MISS %d\n", $remote_miss;
+        printf "REMOTE-HIT %% %f\n", 100*$udp_hit/$udp;
+        printf "REMOTE-HIT %d\n", $udp_hit;
+        printf "REMOTE-MISS %% %f\n", 100*$udp_miss/$udp;
+        printf "REMOTE-MISS %d\n", $udp_miss;
         printf "TCP-HIT %% %f\n", 100*$tcp_hit/$tcp;
         printf "TCP-HIT %d\n", $tcp_hit;
         printf "TCP-MISS %% %f\n", 100*$tcp_miss/$tcp;
@@ -119,5 +135,6 @@ while (<>) {
         printf "DIRECT %d\n", $direct;
         printf "OTHER %% %f\n", 100*$other/$tcp;
         printf "OTHER %d\n", $other;
+  #Here maybe problem to count hit on all tcp and udp plus other?
         printf "ALL_TCP %f\n", ($local_hit+$local_miss+$ims_hit+$mem_hit+$unmodified+$modified+$negative+$aborted_hit+$direct+$other+$sibling_hit)/$N*100;
-        printf "ALL_UDP %f\n", ($remote_hit/$udp+$remote_miss/$udp)*100;
+        printf "ALL_UDP %f\n", ($udp_hit/$udp+$udp_miss/$udp)*100;
